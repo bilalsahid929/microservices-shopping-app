757ab58c777b2341d5611bf00482832eed1fc234
bilalshahid929 <bilalshahid929@gmail.com>
2023-11-30 22:26:16 +0400

    removed unused imports

--- shopping/src/database/index.js
+++ shopping/src/database/index.js
@@ -1,13 +1,6 @@
 // database related modules
 
 import databaseConnection from "./connection.js";
-import ProductRepository from "./repository/product-repository.js";
-import CustomerRepository from "./repository/customer-repository.js";
 import ShoppingRepository from "./repository/shopping-repository.js";
 
-export {
-  databaseConnection,
-  ProductRepository,
-  CustomerRepository,
-  ShoppingRepository,
-};
+export { databaseConnection, ShoppingRepository };
